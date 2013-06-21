e5117475aa3b3f4c6c6e335bc5126f0ea651b398
Zak Grant <zakgrant@gmail.com>
2013-06-21 13:43:57 +0100

    tweak

--- src/main/java/com/github/fge/jsonschema/misc/WebApp.java
+++ src/main/java/com/github/fge/jsonschema/misc/WebApp.java
@@ -40,7 +40,7 @@ public final class WebApp
         //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
+            webPort = "8070";
         }
 
         final Server server = new Server(Integer.valueOf(webPort));
